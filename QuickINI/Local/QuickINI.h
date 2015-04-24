6bce83dab9d614322906a7cee7c5acaa18f5d8cf
Sonickidnextgen <SKIVortex@rocketmail.com>
2015-04-23 18:09:02 -0600

    Made 'QuickINI' header-based, fixed string-management code, etc.

--- QuickINI/Local/QuickINI.h
+++ QuickINI/Local/QuickINI.h
@@ -2,14 +2,26 @@
 
 /*
 	NOTES:
+		* This library is currently header-only. That being said, it's a good idea to reduce includes of this header.
 		* DO NOT mix wide and standard STL technologies, unless you're sure it will compile.
 		For example, wide-streams likely won't support 'std::string' as an input.
 */
 
 // Includes:
 #include <exception>
-#include <iostream>
+#include <algorithm>
+#include <functional>
+
 #include <string>
+
+#include <cctype>
+#include <codecvt>
+//#include <locale>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 #include <map>
 
 // Namespace(s):
@@ -54,46 +66,69 @@ namespace quickLib
 		{
 			public:
 				// Constructor(s):
-				INI_EXCEPTION();
+				INI_EXCEPTION() : exception() { /* Nothing so far. */ }
 
 				// Methods:
-				virtual const string message() const throw();
+				virtual const string message() const throw()
+				{
+					return (string)what();
+				}
 		};
 
 		class operationUnsupported : public INI_EXCEPTION
 		{
 			public:
 				// Constructor(s):
-				operationUnsupported();
+				operationUnsupported() : INI_EXCEPTION() { /* Nothing so far. */ }
 
 				// Methods:
-				virtual const string message() const throw();
+				virtual const string message() const throw()
+				{
+					return (string)"Operation unsupported.";
+				}
 
 				// This currently acts as a standard-compliant wrapper for the 'message' command.
-				virtual const char* what() const throw() override;
+				virtual const char* what() const throw() override
+				{
+					return message().c_str();
+				}
 		};
 
 		class lineError : public INI_EXCEPTION
 		{
 			public:
-				// Constructor(s):
-				lineError(lineNumber_t lineNumber);
-
-				// Methods:
-				virtual const string message() const throw() override;
-
 				// Fields:
 				lineNumber_t error_line;
+
+				// Constructor(s):
+				lineError(lineNumber_t lineNumber) : error_line(lineNumber) { /* Nothing so far. */ }
+
+				// Methods:
+				virtual const string message() const throw() override
+				{
+					stringstream ss;
+
+					ss << "Unknown error on line: " << error_line;
+
+					return ss.str();
+				}
 		};
 
 		class invalidSegment : public lineError
 		{
 			public:
 				// Constructor(s):
-				invalidSegment(lineNumber_t lineNumber);
+				invalidSegment(lineNumber_t lineNumber) : lineError(lineNumber) { /* Nothing so far. */ }
 
 				// Methods:
-				virtual const string message() const throw() override;
+				virtual const string message() const throw() override
+				{
+					stringstream ss;
+
+					ss << "Invalid segment on line: " << error_line;
+
+					return ss.str();
+				}
 
 				// Fields:
 				// Nothing so far.
@@ -103,10 +138,23 @@ namespace quickLib
 		{
 			public:
 				// Constructor(s):
-				variableParseError(lineNumber_t lineNumber, string info="");
+				variableParseError(lineNumber_t lineNumber, string info="")
+					: lineError(lineNumber), information(info) { /* Nothing so far. */ }
 
 				// Methods:
-				virtual const string message() const throw() override;
+				virtual const string message() const throw() override
+				{
+					stringstream ss;
+
+					ss << "Unable to parse variable on line: " << error_line;
+			
+					if (!information.empty())
+					{
+						ss << " - " << information;
+					}
+
+					return ss.str();
+				}
 
 				// Fields:
 				string information;
@@ -115,15 +163,45 @@ namespace quickLib
 		class invalidRightOperand : public variableParseError
 		{
 			public:
-				// Constructor(s):
-				invalidRightOperand(lineNumber_t lineNumber, string left, string right=string());
-
-				// Methods:
-				virtual const string message() const throw() override;
-
 				// Fields:
 				string leftOperand;
 				string rightOperand;
+
+				// Constructor(s):
+				invalidRightOperand(lineNumber_t lineNumber, string left, string right=string())
+					: variableParseError(lineNumber), leftOperand(left), rightOperand(right) { /* Nothing so far. */ }
+
+				// Methods:
+				virtual const string message() const throw() override
+				{
+					stringstream ss;
+
+					ss << "Invalid right operand on line: " << error_line << " | ";
+
+					if (!leftOperand.empty())
+					{
+						ss << "L: " << left << ", ";
+
+						if (!rightOperand.empty())
+						{
+							ss << "R: " << right;
+						}
+						else
+						{
+							ss << "R: NULL";
+						}
+					}
+					else if (!rightOperand.empty())
+					{
+						ss << "L: NULL, R: " << right;
+					}
+					else
+					{
+						ss << "NULL";
+					}
+
+					return ss.str();
+				}
 		};
 
 		// Functions:
@@ -131,8 +209,19 @@ namespace quickLib
 		// String management:
 
 		// Functions:
-		string wideStringToDefault(const wstring wstr);
-		wstring defaultStringToWide(const string str);
+		inline string wideStringToDefault(const wstring wstr)
+		{
+			std::wstring_convert<std::codecvt_utf8<wchar_t>> stringConverter;
+
+			return stringConverter.to_bytes(wstr);
+		}
+		
+		inline wstring defaultStringToWide(const string str)
+		{
+			std::wstring_convert<std::codecvt_utf8<wchar_t>> stringConverter;
+
+			return stringConverter.from_bytes(str);
+		}
 
 		inline void correctString(const string str, wstring& output)
 		{
@@ -182,62 +271,299 @@ namespace quickLib
 			return defaultStringToWide(str);
 		}
 
-		// This trims a string from the beginning.
+		static inline int isquote(int c)
+		{
+			return (int)(c == '\"');
+		}
+
+		// String "cleanup" / trimming code by Evan Teran:
 		template <typename str=string>
-		str ltrim(str s);
+		inline str ltrim(str s)
+		{
+			s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+			s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isquote))));
+			//s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(ispunct))));
 
-		// This trims a string in reverse.
+			return s;
+		}
+
 		template <typename str=string>
-		str rtrim(str s);
+		inline str rtrim(str s)
+		{
+			s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
+			s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isquote))).base(), s.end());
+			//s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(ispunct))).base(), s.end());
 
-		// This trims a string from both ends.
+			return s;
+		}
+
 		template <typename str=string>
-		str trim(str s);
-
-		template <typename INIVars=INIVariables<>>
-		void load(const string& path, INIVars& variables);
-
-		template <typename INIVars=INIVariables<>>
-		void save(const string& path, const INIVars& variables, bool insertTrailingSpaces=false);
-
-		template <typename INIVars=INIVariables<>>
-		void load(const wstring& path, INIVars& variables);
-
-		template <typename INIVars=INIVariables<>>
-		void save(const wstring& path, const INIVars& variables, bool insertTrailingSpaces=false);
-
-		template <typename inputStream=istream, typename INIVars=INIVariables<>>
-		void read(inputStream& is, INIVars& variables);
-
-		template <typename outputStream=ostream, typename INIVars=INIVariables<>>
-		void write(outputStream& os, const INIVars& variables, bool insertTrailingSpaces=false);
+		inline str trim(str s)
+		{
+			return rtrim(ltrim(s));
+		}
 
 		// This command reads an INI entry from the specified input-stream.
 		// The return value is the new line-number, based on the input given.
 		// DO NOT call this on an input-stream that has already ended; please handle this yourself.
-		template <typename inputStream=istream, typename INIVars=INIVariables<>, typename strType=string>
-		lineNumber_t readEntryFromLines(inputStream& is, INIVars& variable, strType line, const lineNumber_t currentLineNumber=0, bool skipStreamCheck=true, const symbols separator=assignmentSymbol, const symbols commentChar=commentSymbol);
-
-		template <typename INIVars=INIVariables<>, typename INISecTag=INISectionTag, typename INISec=INISection<>>
-		void flushVariables(INIVars& variables, INISecTag& currentSection, INISec& currentEntries);
-
-		template <typename strType=string, typename INIVars=INIVariables<>>
-		inline INIVars load(const strType& path)
+		template <typename characterType=char, typename characterTraits=char_traits<characterType>, typename strAlloc=allocator<characterType>>
+		inline lineNumber_t readEntryFromLines
+		(
+			basic_istream<characterType, characterTraits>& is,
+			INIVariable<basic_string<characterType, characterTraits, strAlloc>>& variable,
+			basic_string<characterType, characterTraits, strAlloc> line, const lineNumber_t currentLineNumber=0, const bool skipStreamCheck=true,
+			const symbols separator=assignmentSymbol, const symbols commentChar=commentSymbol
+		)
 		{
-			INIVars output;
+			// Typedefs:
+			typedef basic_string<characterType, characterTraits, strAlloc> strType;
 
+			if (!skipStreamCheck && is.eof())
+			{
+				throw variableParseError(currentLineNumber, "Stream ended.");
+			}
+
+			// Local variable(s):
+			auto comment = line.find(commentChar);
+			
+			// Check for comments:
+			if (comment != strType::npos)
+			{
+				// This line is only a comment, ignore it.
+				if (comment == 0)
+				{
+					getline(is, line);
+					
+					return readEntryFromLines<characterType, characterTraits>(is, variable, line, currentLineNumber+1, false, separator, commentChar);
+				}
+
+				// Change the "scope" of the line to ignore the comment.
+				line = line.substr(0, comment);
+			}
+
+			auto assignment = line.find(separator);
+
+			if (assignment == strType::npos)
+			{
+				variable.first = variable.first + trim(line);
+
+				getline(is, line);
+
+				return readEntryFromLines<characterType, characterTraits>(is, variable, line, currentLineNumber+1, false, separator, commentChar);
+			}
+				
+			if (variable.first.empty())
+			{
+				variable.first = trim(line.substr(0, assignment));
+			}
+			else
+			{
+				variable.first += trim(line.substr(0, assignment));
+			}
+
+			// Now that we're loaded a name for this variable, make sure it's valid:
+			if (variable.first.empty())
+			{
+				throw variableParseError(currentLineNumber, "Invalid variable name.");
+			}
+
+			if ((assignment + 1) < line.length())
+			{
+				variable.second = trim(line.substr(assignment+1));
+			}
+			else
+			{
+				throw invalidRightOperand(currentLineNumber, abstractStringToDefault(variable.first));
+			}
+
+			return currentLineNumber+1;
+		}
+
+		template <typename INISecTag=INISectionTag, typename INISec=INISection<INISecTag>>
+		inline void flushVariables(INIVariables<INISecTag>& variables, const INISecTag& currentSection, INISec& currentEntries)
+		{
+			// Insert the new variable into the 'variables' container:
+			variables[currentSection] = currentEntries;
+			currentEntries = INISec(); // currentEntries.clear();
+
+			return;
+		}
+		
+		template <typename characterType=char, typename characterTraits=char_traits<characterType>, typename strAlloc=allocator<characterType>>
+		inline void read(basic_istream<characterType, characterTraits>& is, INIVariables<basic_string<characterType, characterTraits, strAlloc>>& variables)
+		{
+			// Typedefs:
+			//typedef INIVars::key_type strType;
+			typedef basic_string<characterType, characterTraits, strAlloc> strType;
+
+			// Local variable(s):
+
+			// The global line-number for this read operation.
+			lineNumber_t lineNumber = 1;
+
+			// The current INI section.
+			strType currentSection;
+
+			// Unfortunately, since function templates are picky, I have to use this.
+			correctString("global", currentSection);
+
+			INISection<strType> currentEntries;
+
+			// Read every line of the file:
+			while (!is.eof())
+			{
+				// Local variable(s):
+				strType line;
+				
+				getline<characterType, characterTraits, allocator<characterType>>(is, line);
+
+				if (line.empty())
+					continue;
+
+				auto sectionBegin = line.find(sectionBeginSymbol);
+				
+				// Section tags must be on a single line (Currently):
+				if (sectionBegin != strType::npos)
+				{
+					auto sectionEnd = line.find(sectionEndSymbol);
+
+					// Ensure this is an actual section-tag:
+					if (line.find(commentSymbol) > sectionEnd && line.find(assignmentSymbol) > sectionEnd)
+					{
+						if (sectionEnd != strType::npos)
+						{
+							// Flush the current section, then get the new section-name:
+							flushVariables<strType>(variables, currentSection, currentEntries);
+							currentSection = line.substr(sectionBegin+1, sectionEnd-1);
+						}
+						else
+						{
+							throw invalidSegment(lineNumber);
+						}
+
+						// Check if there's more to the line:
+						if (sectionEnd+1 == line.length())
+							continue;
+
+						// Change the "scope" of the line.
+						line = line.substr(sectionEnd+1);
+					}
+				}
+
+				// The variable-entry we're going to read.
+				INIVariable<strType> variable;
+
+				// Read the entry, then add to the line-number.
+				lineNumber = readEntryFromLines<characterType, characterTraits>(is, variable, line, lineNumber);
+
+				// Insert the newly read entry.
+				currentEntries.insert(variable);
+			}
+
+			// Flush the current variables to the 'variables' container.
+			flushVariables<strType>(variables, currentSection, currentEntries);
+
+			return;
+		}
+		
+		template <typename characterType=char, typename characterTraits=char_traits<characterType>, typename strAlloc=allocator<characterType>>
+		inline void write(basic_ostream<characterType, characterTraits>& os, const INIVariables<basic_string<characterType, characterTraits, strAlloc>>& variables, const bool insertTrailingSpaces=false)
+		{
+			for (auto& section : variables)
+			{
+				os << (characterType)sectionBeginSymbol << section.first << (characterType)sectionEndSymbol << endl;
+
+				for (auto& entry : section.second)
+				{
+					os << entry.first << " " << (characterType)assignmentSymbol << " " << entry.second << endl;
+				}
+
+				if (insertTrailingSpaces)
+				{
+					os << endl;
+				}
+			}
+
+			return;
+		}
+		
+		inline void load(const string& path, INIVariables<string>& variables)
+		{
+			ifstream f;
+
+			f.exceptions(ifstream::failbit | ifstream::badbit);
+
+			f.open(path);
+
+			read(f, variables);
+
+			f.close();
+
+			return;
+		}
+		
+		inline void save(const string& path, const INIVariables<string>& variables, const bool insertTrailingSpaces=false)
+		{
+			ofstream f;
+			
+			f.exceptions(ofstream::failbit | ofstream::badbit);
+
+			f.open(path);
+			
+			write(f, variables, insertTrailingSpaces);
+			
+			f.close();
+			
+			return;
+		}
+
+		inline void load(const wstring& path, INIVariables<wstring>& variables)
+		{
+			wifstream f;
+
+			f.exceptions(wifstream::failbit | wifstream::badbit);
+
+			f.open(path);
+			
+			read<wstring::value_type, wstring::traits_type>(f, variables);
+
+			f.close();
+
+			return;
+		}
+		
+		inline void save(const wstring& path, const INIVariables<wstring>& variables, const bool insertTrailingSpaces=false)
+		{
+			wofstream f;
+			
+			f.exceptions(wofstream::failbit | wofstream::badbit);
+
+			f.open(path);
+			
+			write<wstring::value_type, wstring::traits_type>(f, variables, insertTrailingSpaces);
+			
+			f.close();
+			
+			return;
+		}
+		
+		template <typename strType=string>
+		inline INIVariables<strType> load(const strType& path)
+		{
+			INIVariables<strType> output;
+			
 			load(path, output);
-
+			
 			return output;
 		}
 
-		template <typename inputStream=istream, typename INIVars=INIVariables<basic_string<inputStream::char_type>>>
-		inline INIVars read(inputStream& is)
+		template <typename characterType=char, typename characterTraits=char_traits<characterType>, typename strAlloc=allocator<characterType>>
+		inline INIVariables<basic_string<characterType, characterTraits, strAlloc>> read(basic_istream<characterType, characterTraits>& is)
 		{
-			INIVars output;
-
+			INIVariables<basic_string<characterType, characterTraits, strAlloc>> output;
+			
 			read(is, output);
-
+			
 			return output;
 		}
 	}
---
+++ QuickINI/QuickINI_Example.cpp
@@ -0,0 +1,57 @@
+// Preprocessor related:
+//#define QINI_LOCAL_BUILD
+
+// Includes:
+#ifdef QINI_LOCAL_BUILD
+	#include "Local/QuickINI.h"
+#else
+	#include <QuickLib/QuickINI/QuickINI.h>
+#endif
+
+/*
+int main()
+{
+	// Namespace(s):
+	using namespace std;
+	using namespace quickLib;
+
+	wstring file = L"X\t\t\t=Y\nZ=  W\nA = B\nC =D\n[Test]O_O=\"[10, 15, 20, 25]\" ; Hello world.\nLife=42\nYou know what they say         = Hello World ;;;;.";
+
+	wstringstream is(file);
+
+	INI::INIVariables<wstring> data;
+
+	try
+	{
+		data = INI::read(is);
+
+		for (auto& section : data)
+		{
+			wcout << '[' << section.first << ']' << endl;
+
+			for (auto& e : section.second)
+			{
+				wcout << '\'' << e.first << '\'' << " = " << '\'' << e.second << '\'' << endl;
+			}
+
+			wcout << endl;
+		}
+	}
+	catch (const INI::INI_EXCEPTION& e)
+	{
+		cout << "Exception caught: " << e.message() << endl;
+
+		system("PAUSE");
+
+		return -1;
+	}
+
+	cout << "------------------------" << endl;
+
+	INI::write(wcout, data, true);
+
+	system("PAUSE");
+
+	return 0;
+}
+*/
